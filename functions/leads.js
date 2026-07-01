8eb9d3e1f125ea05a651169738049e8f51282e0d
Webmaster <42879137+Readeasy30@users.noreply.github.com>
2026-06-30 21:46:14 -0500

    Add onRequestGet function to retrieve leads

--- functions/leads.js
+++ functions/leads.js
@@ -1,1 +1,8 @@
+export async function onRequestGet(context) {
+  let leads = await context.env.LEADS.get("leads");
+  leads = leads ? JSON.parse(leads) : [];
 
+  return new Response(JSON.stringify({ leads }), {
+    headers: { "Content-Type": "application/json" }
+  });
+}
